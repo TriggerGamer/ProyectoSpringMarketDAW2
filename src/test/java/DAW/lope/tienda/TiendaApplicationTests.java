e520f29dcc82c75adb643c2e26ebbac752ef7a2a
TriggerGamer <ejaquez@educa.madrid.org>
2021-10-18 18:09:28 +0200

    Primer dia

---
+++ src/test/java/DAW/lope/tienda/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package DAW.lope.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
