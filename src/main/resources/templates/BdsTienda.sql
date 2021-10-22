5b8b98216ebd6d01fb56451a3300dc6caada3101
Sicotronicoo <martinezholguinluis@gmail.com>
2021-10-22 19:10:02 +0200

    mas cabioos

--- src/main/resources/templates/BdsTienda.sql
+++ src/main/resources/templates/BdsTienda.sql
@@ -9,11 +9,15 @@ precio double(10,2),
 descuento int(7)
 );
 
-INSERT INTO Productos(tituloProductProductoso, descripcionProducto, precio, descuento) VALUES("Ordenador Sobremesa", "Ordenador con i5, 16GB de RAM, Gráfica 1080ti con 2GB de RAM integrados, buena ventilacion y carcasa Krom set 12-33", 500.99, 10);
 INSERT INTO Productos(tituloProducto, descripcionProducto, precio, descuento) VALUES("Lenovo IdeaCentre","Diseñado específicamente para catapultarte al mundo de los videojuegos y los deportes electrónicos, esta formidable torre desatará tus opciones de productividad y creatividad",757.07,7);
 INSERT INTO Productos(tituloProducto, descripcionProducto, precio, descuento) VALUES("Silver AMD Ryzen 5","La nueva línea de Pcs de sobremesa para juegos creada tras un amplio trabajo de investigación para ofrecer el mayor rendimiento y optimización en el juego para nuestros cliente",1347.42,2);
 INSERT INTO Productos(tituloProducto, descripcionProducto, precio, descuento) VALUES("MSI GF75 Thin","Te presentamos el portátil GF75 Thin de MSI, un portátil gaming con procesador Intel Core i7, 16GB de RAM, 512GB de SSD y gráfica Nvidia GeForce RTX 3060.",1227.34,0);
 INSERT INTO Productos(tituloProducto, descripcionProducto, precio, descuento) VALUES("MSI GS75 Stealth","Prepárate para sentir todo el poder del juego con el potente ordenador portátil de MSI GS75 Stealth. Un portátil gaming dotado con un procesador Coffeelake i7, 32GB de RAM, 1TB SSD de almacenamiento, y todo bajo una potente gráfica NVIDIA GeForce® RTX 2060 GDDR6 6GB. ",1477.59,2);
+INSERT INTO Productos(tituloProducto, descripcionProducto, precio, descuento) VALUES("PNY Quadro RTX 6000","NVIDIA® Quadro RTX? 6000, impulsada por la arquitectura NVIDIA Turing y la plataforma NVIDIA RTX, ofrece el avance más importante de las últimas dos décadas en gráficos por ordenador para los flujos de trabajo profesionales.",4519.14,0);
+INSERT INTO Productos(tituloProducto, descripcionProducto, precio, descuento) VALUES("Asus Rog Swift PG32UQX","ROG Swift PG32UQX. EL PRIMER MINI MONITOR LED PARA JUEGOS DEL MUNDO. El ROG Swift PG32UQX es el primer monitor para juegos 4K 144 Hz NVIDIA® G-SYNC® Ultimate.",3369,2);
+INSERT INTO Productos(tituloProducto, descripcionProducto, precio, descuento) VALUES("Razer BlackWidow V3 Pro","El primer y más icónico teclado mecánico para juegos del mundo hace su próxima evolución revolucionaria. Entra a un nuevo meta inalámbrico con el Razer BlackWidow V3 Pro.",329.99,5);
+INSERT INTO Productos(tituloProducto, descripcionProducto, precio, descuento) VALUES("Sony PlayStation 5 Standard + Ratchet & Clank","PlayStation® 5: Jugar no tiene límites. Experimenta cargas superrápidas gracias a una unidad de estado sólido (SSD) de alta velocidad, una inmersión más profunda con retroalimentación háptica, gatillos adaptables y audio 3D.",579.99,10);
+
 
 select * from Productos;
 
