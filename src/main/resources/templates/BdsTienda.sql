e661186dd6147d1dda44bf037c7c57380c4438f2
TriggerGamer <ejaquez@educa.madrid.org>
2021-11-08 17:41:31 +0100

    buenas chacho

--- src/main/resources/templates/BdsTienda.sql
+++ src/main/resources/templates/BdsTienda.sql
@@ -29,10 +29,9 @@ titularTarjeta VARCHAR (100),
 codigoSeguridad INT(3),
 direccionFacturacion VARCHAR (200)
 );
-INSERT INTO Usuarios VALUES(1,"luis","Martinez", 'luis',"luis@.com","1994-12-12","12345678","luis","123","garcia plata de osma");
-INSERT INTO Usuarios VALUES(2,"Luis","Martinez", 'contrafulanito',"luis@.com","1994-12-12","12345678","luis","123","garcia plata de osma");
 
-SELECT * FROM Usuarios;
+INSERT INTO Usuarios VALUES(2,"luis","Martinez", 'luis',"luis@.com","1994-12-12","12345678","luis","123","garcia plata de osma");
+INSERT INTO Usuarios VALUES(1,"Luis","Martinez", 'contrafulanito',"luis@.com","1994-12-12","12345678","luis","123","garcia plata de osma");
 
 delete from Usuarios;
 
