28c8970e942b883c4e09051fb093d1d3e5d6520c
Sicotronicoo <martinezholguinluis@gmail.com>
2021-11-15 20:00:39 +0100

    compras

--- src/main/java/DAW/lope/tienda/modelo/Carrito.java
+++ src/main/java/DAW/lope/tienda/modelo/Carrito.java
@@ -4,16 +4,14 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Carrito implements Serializable {
-	private int id_Pedido;
-	private int id_Usuario;
 	private int id_Producto;
 	private String nombreProducto;
 	private int numeroUnidades;
-	private String fechaDePedido;
-	
-	public Carrito(){}
-	
-	public Carrito(int id_producto, String nombre, int unidades){
+
+	public Carrito() {
+	}
+
+	public Carrito(int id_producto, String nombre, int unidades) {
 		this.id_Producto = id_producto;
 		this.nombreProducto = nombre;
 		this.numeroUnidades = unidades;
@@ -43,29 +41,4 @@ public class Carrito implements Serializable {
 		this.numeroUnidades = numeroUnidades;
 	}
 
-	public int getId_Pedido() {
-		return id_Pedido;
-	}
-
-	public void setId_Pedido(int id_Pedido) {
-		this.id_Pedido = id_Pedido;
-	}
-
-	public int getId_Usuario() {
-		return id_Usuario;
-	}
-
-	public void setId_Usuario(int id_Usuario) {
-		this.id_Usuario = id_Usuario;
-	}
-
-	public String getFechaDePedido() {
-		return fechaDePedido;
-	}
-
-	public void setFechaDePedido(String fechaDePedido) {
-		this.fechaDePedido = fechaDePedido;
-	}
-	
-	
 }
---
+++ src/main/java/DAW/lope/tienda/modelo/Compras.java
@@ -0,0 +1,46 @@
+package DAW.lope.tienda.modelo;
+
+public class Compras {
+	
+	private int id_Pedido;
+	private int id_Usuario;
+	private int id_Producto;
+	private int numeroUnidades;
+	private String fechaDePedido;
+
+	
+	public Compras (){
+		
+	}
+	public Compras (int id_Pedido, int id_Usuario, int id_Producto, int numeroUnidades, String fechaDePedido) {
+		this.id_Pedido = id_Pedido;
+		this.id_Usuario = id_Usuario;
+		this.id_Producto = id_Producto;
+		this.numeroUnidades = numeroUnidades;
+		this.fechaDePedido = fechaDePedido;
+	}	
+	
+	public int getId_Pedido() {
+		return id_Pedido;
+	}
+
+	public void setId_Pedido(int id_Pedido) {
+		this.id_Pedido = id_Pedido;
+	}
+
+	public int getId_Usuario() {
+		return id_Usuario;
+	}
+
+	public void setId_Usuario(int id_Usuario) {
+		this.id_Usuario = id_Usuario;
+	}
+
+	public String getFechaDePedido() {
+		return fechaDePedido;
+	}
+
+	public void setFechaDePedido(String fechaDePedido) {
+		this.fechaDePedido = fechaDePedido;
+	}
+}
