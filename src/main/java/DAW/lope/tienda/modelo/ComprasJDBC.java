441744017ff4ab2568916556d1d6ba84590dde6a
Sicotronicoo <martinezholguinluis@gmail.com>
2021-11-18 19:15:52 +0100

    cambioss

--- src/main/java/DAW/lope/tienda/modelo/ComprasJDBC.java
+++ src/main/java/DAW/lope/tienda/modelo/ComprasJDBC.java
@@ -14,24 +14,24 @@ public class ComprasJDBC implements ComprasDao {
 
 	@Override
 	public int save(Compra compra) {
-		return jdbcTemplate.update("INSERT INTO Compra(id_Usuario, fechaDeCompra) VALUES(?,NOW())",
+		return jdbcTemplate.update("INSERT INTO Compras(id_Usuario, fechaDeCompra) VALUES(?,NOW())",
 				compra.getId_Usuario());
 	}
 	
 	@Override
 	public int saveProductosCompra(int id_Compra, int id_Producto, int  numeroUnidades) {
-		return jdbcTemplate.update("INSERT INTO ProductosCompra VALUES(?,?,?)",
+		return jdbcTemplate.update("INSERT INTO ProductosCompras VALUES(?,?,?)",
 				id_Compra, id_Producto, numeroUnidades);
 	}
 
 	@Override
 	public int deleteById(int id) {
-		return jdbcTemplate.update("DELETE FROM Compra WHERE id_Compra = " + id);
+		return jdbcTemplate.update("DELETE FROM Compras WHERE id_Compra = " + id);
 	}
 
 	@Override
 	public List<Compra> findAll(int id) {
-		return jdbcTemplate.query("SELECT * FROM Compra Where id_Usuario  = " + id + " ORDER BY id_Compra DESC", (rs,
+		return jdbcTemplate.query("SELECT * FROM Compras Where id_Usuario  = " + id + " ORDER BY id_Compra DESC", (rs,
 				rowNum) -> new Compra(rs.getInt("id_Compra"), rs.getInt("id_Usuario"), rs.getString("fechaDeCompra")));
 	}
 
