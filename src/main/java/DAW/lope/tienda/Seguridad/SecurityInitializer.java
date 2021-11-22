790128f6ab8fc79136f6e14eaa8eb68e75c0467a
TriggerGamer <ejaquez@educa.madrid.org>
2021-11-22 19:21:47 +0100

    mucho texto

---
+++ src/main/java/DAW/lope/tienda/Seguridad/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package DAW.lope.tienda.Seguridad;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
